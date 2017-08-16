715611f5c721189521ea3d16326cc0ae4a9985f8
Saalihou Ndiaye <saalihou@gmail.com>
2017-08-16 20:24:01 +0000

    fix: Corriger le panning de l'Activity a l'ouverture du clavier

--- android/app/src/main/java/com/agromarket/MainApplication.java
+++ android/app/src/main/java/com/agromarket/MainApplication.java
@@ -9,6 +9,13 @@ import com.reactnativenavigation.NavigationApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 
+import com.reactnativenavigation.controllers.ActivityCallbacks;
+
+import android.view.WindowManager;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
 public class MainApplication extends NavigationApplication {
 
     @Override
@@ -29,6 +36,46 @@ public class MainApplication extends NavigationApplication {
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
         return getPackages();
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        setActivityCallbacks(new ActivityCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
     }
 }
 

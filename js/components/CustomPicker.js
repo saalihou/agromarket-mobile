6d8cae624a07d68cdd56e5eddbd99d8d907a9834
Saalihou Ndiaye <saalihou@gmail.com>
2017-08-17 23:36:51 +0000

    chore: Formatter CustomPicker

--- js/components/CustomPicker.js
+++ js/components/CustomPicker.js
@@ -3,48 +3,56 @@ import { Picker, StyleSheet, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class CustomPicker extends Component {
-    static defaultProps = {
-        items: []
-    }
-    render() {
-        const { items } = this.props;
-        return(
-            <View style={styles.container}>
-                <View style={styles.icon}>
-                    <Icon name={this.props.icon} color='white' size={25} />
-                </View>
-                <Picker itemStyle={styles.item} {...this.props} style={[styles.picker, this.props.style]}>
-                    {items.map(item => (
-                        <Picker.Item label={item.label} value={item.value} key={item.value} />
-                    ))}
-                </Picker>
-            </View>
-        )
-    }
+  static defaultProps = {
+    items: []
+  };
+  render() {
+    const { items } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.icon}>
+          <Icon name={this.props.icon} color="white" size={25} />
+        </View>
+        <Picker
+          itemStyle={styles.item}
+          {...this.props}
+          style={[styles.picker, this.props.style]}
+        >
+          {items.map(item =>
+            <Picker.Item
+              label={item.label}
+              value={item.value}
+              key={item.value}
+            />
+          )}
+        </Picker>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'stretch',
-        marginBottom: 1,
-    },
-    picker: {
-        flex: 1,
-        color: 'white',
-        backgroundColor: 'rgba(0,0,0,0.4)',
-        borderTopRightRadius: 4,
-        borderBottomRightRadius: 4,
-        height: 57
-    },
-    item: {
-        fontWeight: 'bold'
-    },
-    icon: {
-        borderTopLeftRadius: 4,
-        borderBottomLeftRadius: 4,
-        backgroundColor: "rgba(0,0,0,0.3)",
-        justifyContent: 'center',
-        padding: 15
-    }
-})
+  container: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginBottom: 1
+  },
+  picker: {
+    flex: 1,
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    borderTopRightRadius: 4,
+    borderBottomRightRadius: 4,
+    height: 57
+  },
+  item: {
+    fontWeight: 'bold'
+  },
+  icon: {
+    borderTopLeftRadius: 4,
+    borderBottomLeftRadius: 4,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    justifyContent: 'center',
+    padding: 15
+  }
+});
